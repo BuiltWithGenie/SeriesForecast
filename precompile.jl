f7ee92200255c49cd7b918da7a2a86fe06b7c373
PGimenez <gimenezfebrer@gmail.com>
2024-02-05 10:17:17 +0100

    updated precompile script

--- precompile.jl
+++ precompile.jl
@@ -1,6 +1,9 @@
 
 using GenieFramework
-using HTTP
-Genie.loadapp()
-up(async=true)
-HTTP.get("http://localhost:8000/");
+include("lib/NodeUtils.jl")
+include("lib/Delhi.jl")
+include("lib/utils.jl")
+#= using HTTP =#
+#= Genie.loadapp() =#
+#= up(async=true) =#
+#= HTTP.get("http://localhost:8000/"); =#
